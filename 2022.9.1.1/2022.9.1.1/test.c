c5c268e810347bddc6c611336fb1f0f59a7b527b
MAXLEE1231 <15605661952@163.COM>
2022-09-02 22:17:40 +0800

    代码上传
    Signed-off-by: MAXLEE1231 <15605661952@163.COM>

---
+++ 2022.9.1.1/2022.9.1.1/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+//#define MALLOC(X,Y) (Y*)malloc(X*sizeof(Y))
+//
+//int main()
+//{
+//	MALLOC(10, int);
+//	
+//	return 0;
+//}
+
+
+//#define M 100
+//
+//int main()
+//{
+//	int a = M;
+//#undef M
+//	printf("%d", a);
+//	printf("%d", M);
+//	return 0;
+//}
+
